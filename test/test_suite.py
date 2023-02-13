47e3d73bd32ab7e76feaf5debb8af82bb9b8fa44
gcwill70 <gcwill70@gmail.com>
2023-02-13 15:09:20 -0500

    improve local tests

--- test/test_suite.py
+++ test/test_suite.py
@@ -2,84 +2,108 @@ import os
 from main import main
 from unittest import mock
 from pytest import raises
+from actions_toolkit import core
+
 
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
-    })
+})
 def test_basic():
     main()
 
+
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/nonexistent-repo",
-    })
+})
 def test_basic_fail():
     with raises(SystemExit) as e:
         main()
 
+
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
-    "INPUT_AFTER": '2022-04-14T11:59:59-04:00',
-    "INPUT_BEFORE": '2022-04-26T11:59:59-04:00',
-    })
+    "INPUT_AFTER": '2022/03/28 11:59:59 GMT',
+    "INPUT_BEFORE": 'Wed, 27 Apr 2022 01:54:37 GMT',
+})
+def test_bad_timestamp():
+    with raises(SystemExit) as e:
+        main()
+
+
+@mock.patch.dict(os.environ, {
+    "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
+    "INPUT_AFTER": '2022-04-25T11:59:59-04:00',
+    "INPUT_BEFORE": '2022-04-27T11:59:59-04:00',
+})
 def test_windowed():
     main()
+    assert os.environ['OUTPUT_COMMIT'] == '61d8b4ee576df170757e2863c565a274699af2a9'
+
 
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_AFTER": '2022-03-28T11:59:59-04:00',
     "INPUT_BEFORE": '2022-04-14T11:59:59-04:00',
-    })
+})
 def test_windowed_nocommit_fail():
     with raises(SystemExit) as e:
         main()
+
 
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_AFTER": '2022-04-02T11:59:59-04:00',
     "INPUT_BEFORE": '2022-04-01T11:59:59-04:00',
-    })
+})
 def test_windowed_invalid_fail():
     with raises(SystemExit) as e:
         main()
 
+
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_TAG": "v1",
-    })
+})
 def test_tagged():
     main()
+    assert os.environ['OUTPUT_COMMIT'] == '61d8b4ee576df170757e2863c565a274699af2a9'
+
 
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_TAG": "v1",
-    "INPUT_AFTER": "Wed, 27 Apr 2022 01:54:35 GMT",
-    "INPUT_BEFORE": "Wed, 27 Apr 2022 01:54:37 GMT",
-    })
+    "INPUT_AFTER": "2022-04-27T01:54:35-00:00",
+    "INPUT_BEFORE": "2022-04-27T01:54:37-00:00",
+})
 def test_tagged_timewindow():
     main()
+    assert os.environ['OUTPUT_COMMIT'] == '61d8b4ee576df170757e2863c565a274699af2a9'
+
 
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_TAG": "v1",
-    "INPUT_AFTER": "Wed, 27 Apr 2022 01:54:37 GMT",
-    })
+    "INPUT_AFTER": "2022-04-27T01:54:37-00:00",
+})
 def test_tagged_timewindow_notfound():
     with raises(SystemExit) as e:
         main()
 
+
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_TAG": "v0",
-    })
+})
 def test_tagged_notag():
     with raises(SystemExit) as e:
         main()
+
 
 @mock.patch.dict(os.environ, {
     "INPUT_REPOSITORY": "PurdueECE/action-find-commit",
     "INPUT_SHA": "main",
     "INPUT_TAG": "v1",
-    })
+})
 def test_sha_tagged_fail():
     with raises(SystemExit) as e:
         main()
